603f0374380ebac9dc0e3a6f051b0c5a2a536fa2
Yori Adi Atma <yoriadiatma@gmail.com>
2025-05-20 09:13:13 +0700

    homepage

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.template')
+
+@section('content')
+<h1>Latest Movies</h1>
+
+<div class="row">
+    {{-- Looping data movie --}}
+    @foreach ($movies as $movie)    
+    <div class="col-lg-6">
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="{{ $movie->cover_image }}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <p class="card-text">{{ Str::words($movie->synopsis, 20, '...') }}</p>
+                        <a href="#" class="btn btn-success">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
+    {{-- End looping data movie --}}
+    {{ $movies->links() }}
+</div>
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MovieController::class, 'homepage']);
